0c5d90d8acc7cd3f9bdb762e1beac856e284da28
NamanKumar2501 <namankumar246727@gmail.com>
2023-02-02 12:00:48 +0530

    Work on Chat Activity

--- app/src/main/java/com/example/whatsappclone/ChatDetailActivity.java
+++ app/src/main/java/com/example/whatsappclone/ChatDetailActivity.java
@@ -1,15 +1,26 @@
  package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.whatsappclone.Adapters.ChatAdapter;
+import com.example.whatsappclone.Models.MessageModel;
 import com.example.whatsappclone.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
 
  public class ChatDetailActivity extends AppCompatActivity {
 
@@ -28,7 +39,7 @@ import com.squareup.picasso.Picasso;
         database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
 
-        String senderId = auth.getUid();
+        final String senderId = auth.getUid();
         String recieveId = getIntent().getStringExtra("userId");
         String userName = getIntent().getStringExtra("userName");
         String profilePic = getIntent().getStringExtra("profilePic");
@@ -46,6 +57,70 @@ import com.squareup.picasso.Picasso;
             }
         });
 
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+        final ChatAdapter chatAdapter = new ChatAdapter(messageModels, this);
+
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+
+        final  String senderRoom = senderId + recieveId;
+        final  String receiverRoom = recieveId + senderId;
+
+        database.getReference().child("chats")
+                        .child(senderRoom)
+                        .addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    messageModels.clear();
+                                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                                    MessageModel model = snapshot1.getValue(MessageModel.class);
+
+                                    messageModels.add(model);
+                                }
+                                chatAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+
+
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+             String message = binding.etMessage.getText().toString();
+             final  MessageModel model = new MessageModel(senderId , message);
+             model.setTimestamp(new Date().getTime());
+             binding.etMessage.setText("");
+
+             database.getReference().child("chats")
+                     .child(senderRoom)
+                     .push()
+                     .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                         @Override
+                         public void onSuccess(Void unused) {
+                             database.getReference().child("chats")
+                                     .child(receiverRoom)
+                                     .push()
+                                     .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                         @Override
+                                         public void onSuccess(Void unused) {
+
+                                         }
+                                     });
+                         }
+                     });
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/whatsappclone/Models/MessageModel.java
+++ app/src/main/java/com/example/whatsappclone/Models/MessageModel.java
@@ -1,4 +1,46 @@
 package com.example.whatsappclone.Models;
 
 public class MessageModel {
+
+    String uTd, message;
+    Long timestamp;
+
+    public MessageModel(String uTd, String message, Long timestamp) {
+        this.uTd = uTd;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public MessageModel(String uTd, String message) {
+        this.uTd = uTd;
+        this.message = message;
+    }
+
+    public  MessageModel(){
+        // Empty Constructor
+    }
+
+    public String getuTd() {
+        return uTd;
+    }
+
+    public void setuTd(String uTd) {
+        this.uTd = uTd;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
